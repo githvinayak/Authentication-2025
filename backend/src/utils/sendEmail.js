de2ad385752dd7385725df68fa0b6b81531f6b92
githvinayak <vinayak192002@gmail.com>
2025-03-20 12:37:48 +0530

    added sendMail function

---
+++ backend/src/utils/sendEmail.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+
+export const sendEmail = async (to, subject, html) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.EMAIL_USER, // Your email
+        pass: process.env.EMAIL_PASS, // Your email password or app password
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log(`Email sent to ${to}`);
+  } catch (error) {
+    console.error("Email sending error:", error);
+    throw new Error("Email sending failed");
+  }
+};
