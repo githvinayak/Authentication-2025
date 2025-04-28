ae54e2c49670c4e02b9b64107c083a126ab9248c
githvinayak <vinayak192002@gmail.com>
2025-04-28 18:08:29 +0530

    modified

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     <div>
       <Header />
       <main className="p-6">
-        <h2 className="text-2xl font-semibold">Welcome to the Home Page!</h2>
+        <h2 className="text-2xl sca font-semibold">Welcome to the Home Page!</h2>
         <p className="mt-2 text-gray-600">You're logged in 🎉</p>
       </main>
     </div>
