24e01e20cacb988d8b1b7345f6af408fb53cfc5a
githvinayak <vinayak192002@gmail.com>
2025-04-29 18:16:24 +0530

    modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,32 @@
-import { Button } from "@/components/ui/button"
- 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Verify from "./pages/Verify";
+import Settings from "./pages/Settings";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import Disable2FA from "./pages/Disable2FA";
+import Verify2FA from "./pages/Verify2FA";
+import VerifyLogin2FA from "./pages/VerifyLogin2FA";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/verify-email" element={<Verify />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/disable-2fa" element={<Disable2FA />} />
+        <Route path="/verify-2fa" element={<Verify2FA />} />
+        <Route path="/verify-login-2fa" element={<VerifyLogin2FA />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
- 
-export default App
+
+export default App;
